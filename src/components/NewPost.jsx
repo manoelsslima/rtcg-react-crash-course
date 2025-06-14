9ba6df7e9c6b11cb98626b3819d8103644e78055
Manoel Lima <manoelsslima@yahoo.com.br>
2025-06-14 00:07:12 -0400

    feat: Working with State

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,13 +1,22 @@
 import classes from './NewPost.module.css';
+import {useState} from "react";
 
 function NewPost() {
+
+    // const stateData = useState('');
+    // stateData has two elements (because useState returns an array)
+    // stateData[0] // current value
+    // stateData[1] // state updating function
+    // when we call update function, React will rerender the component with "current value" updated
+
+    const [enteredBody, setEnteredBody ] = useState('');
 
     /* will be passed to onChange event
     equivalent to:
     addEventListener('change'), function(event) {})
     */
     function changeBodyHandler(event) {
-        console.log(event.target.value);
+        setEnteredBody(event.target.value);
     }
 
   // "htmlFor" replaces "for" attribute in React 
@@ -17,6 +26,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
