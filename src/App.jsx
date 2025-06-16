c56b32c688a57ee191cbf6497feb8af840b6a7c9
Manoel Lima <manoelsslima@yahoo.com.br>
2025-06-15 23:45:23 -0400

    feat: adding shared header & more state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import PostList from "./components/PostsList";
+import MainHeader from "./components/MainHeader.jsx";
+import {useState} from "react";
 
 // const list = [{nome:"manoel", texto:"React.js é legal"}, {nome:"Maximilian", texto:"Confira o novo curso"}];
 
 function App() {
-  return (
-    <main>
-      <PostList />
-    </main>
-  );
+    const [modalIsVisible, setModalIsVisible] = useState(false);
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+
+    function showModalHandler() {
+        setModalIsVisible(true);
+    }
+
+    return (
+        <>
+            <MainHeader onCreatePost={showModalHandler} />
+            <main>
+                <PostList isPosting={modalIsVisible} onStopPosting={hideModalHandler}/>
+            </main>
+        </>
+    );
 }
 
 export default App;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,11 +4,10 @@ import classes from './PostsList.module.css';
 import {useState} from "react";
 import Modal from "./Modal";
 
-function PostList() {
+function PostList(props) {
 
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
-    const [modalIsVisible, setModalIsVisible] = useState(true);
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -18,16 +17,12 @@ function PostList() {
         setEnteredAuthor(event.target.value);
     }
 
-    function hideModalHandler() {
-        setModalIsVisible(false);
-    }
-
     return (
         <>
-            {modalIsVisible === true ?
-                <Modal onClose={hideModalHandler}> {/* NewPost component will be received as a props.children on Modal component*/}
+            {props.isPosting === true ?
+                <Modal onClose={props.onStopPosting}> {/* NewPost component will be received as a props.children on Modal component*/}
                     <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
-                </Modal> : null // or false
+                </Modal> : false // or false
             }
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
